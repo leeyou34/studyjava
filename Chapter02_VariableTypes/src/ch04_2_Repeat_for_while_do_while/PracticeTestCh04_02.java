fcf76f3fafec9af28ddbd46fa646abcc73898052
tomle <tomle@I205s-M>
2021-07-28 18:14:28 +0900

    work from July 28th

--- Chapter02_VariableTypes/src/ch04_2_Repeat_for_while_do_while/PracticeTestCh04_02.java
+++ Chapter02_VariableTypes/src/ch04_2_Repeat_for_while_do_while/PracticeTestCh04_02.java
@@ -5,13 +5,20 @@ public class PracticeTestCh04_02 {
 	public static void main(String[] args) {
 		// question 5.
 
-		for(int x =0; x <= 4 ; x++) {
-			for(int y= 0; y <=x; y++) {
-				System.out.print("*");
+//		for(int x =0; x <= 4 ; x++) {
+//			for(int y= 0; y <=x; y++) {
+//				System.out.print("*");
+//		}
+//			System.out.println();
+//		}
+		
+		for(int xe =0; xe <= 4 ; xe++) {
+			for(int ye= 0; ye <=xe; ye++) {
+				System.out.printf("%-4s","*");
 		}
-			System.out.println();
-		}
+			System.out.printf("%s","\n");
 		
 	}
 
+	}
 }
