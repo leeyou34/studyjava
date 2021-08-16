dc7101a99ae47b077322959d0fa24fa718b21207
leeyou31 <81919273+leeyou31@users.noreply.github.com>
2021-08-17 00:10:51 +0900

    Add files via upload
    Aug 17th midnight

---
+++ Chapter02_VariableTypes/src/ch06_5_instance_and_static_member/Earth.java
@@ -0,0 +1,6 @@
+package ch06_5_instance_and_static_member;
+
+public class Earth {
+	static final double EARTH_RADIUS = 6400;
+	static final double EARTH_AREA = 4 * Math.PI * EARTH_RADIUS * EARTH_RADIUS;
+}
---
+++ Chapter02_VariableTypes/src/ch06_5_instance_and_static_member/EarthExample.java
@@ -0,0 +1,11 @@
+package ch06_5_instance_and_static_member;
+
+public class EarthExample {
+
+	public static void main(String[] args) {
+		System.out.println("Earth's radius: " + Earth.EARTH_RADIUS + "km");
+		System.out.println("Earth's area: " + Earth.EARTH_AREA + "km");
+
+	}
+
+}
