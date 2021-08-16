9af3675875ce22abbcc906ab44dd1ddbcb04c0f3
leeyou31 <81919273+leeyou31@users.noreply.github.com>
2021-08-16 23:08:50 +0900

    Add files via upload
    aug 16th work

---
+++ Chapter02_VariableTypes/src/ch06_3_constructor/PracticeTestCh06_3_4.java
@@ -0,0 +1,14 @@
+package ch06_3_constructor;
+
+public class PracticeTestCh06_3_4 {
+
+	public static void main(String[] args) {
+		PracticeTestCh06_3_3 board1 = new PracticeTestCh06_3_3(/*"title", "content"*/);
+		PracticeTestCh06_3_3 board2 = new PracticeTestCh06_3_3(/*"title". "content", "writer"*/);
+		PracticeTestCh06_3_3 board3 = new PracticeTestCh06_3_3(/*"title". "content", writer", "date"*/);
+		PracticeTestCh06_3_3 board4 = new PracticeTestCh06_3_3(/*"title". "content", writer", "date", "hitcount"*/);
+//		System.out.println();
+		
+	}
+
+}
