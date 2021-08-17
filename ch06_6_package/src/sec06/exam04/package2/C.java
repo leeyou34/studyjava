4889101960ac4f121cbdafd3afa3427b777e62c0
leeyou31 <81919273+leeyou31@users.noreply.github.com>
2021-08-17 19:23:46 +0900

    Add files via upload
    aug 17th updated

--- ch06_6_package/src/sec06/exam04/package2/C.java
+++ ch06_6_package/src/sec06/exam04/package2/C.java
@@ -1,12 +1,10 @@
 package sec06.exam04.package2;
 
-import sec06.exam04.package1.A;
-
-
+import sec06.exam05.package1.A;
 
 public class C {
 	//field
-	A a1 = new A(true);
-	A a2 = new A(1);
-	A a3 = new A("string");
+	A a1 = new A(/*true*/);
+	//A a2 = new A(1); // default constructor can't access
+	//A a3 = new A("string"); // private constructor can't access
 }
