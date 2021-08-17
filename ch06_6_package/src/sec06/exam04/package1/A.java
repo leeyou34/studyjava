1813a12aad694cd58f0b3420978e1587c7ce2724
leeyou31 <81919273+leeyou31@users.noreply.github.com>
2021-08-17 19:22:46 +0900

    Add files via upload
    aug 17th updated

--- ch06_6_package/src/sec06/exam04/package1/A.java
+++ ch06_6_package/src/sec06/exam04/package1/A.java
@@ -7,7 +7,17 @@ public class A {
 	A a3 = new A("string");
 	
 	//constructor
-	public A(boolean b) {}
-	A(int b) {}
-	private A(String s) {}
+	public A(boolean b) {} //restrict the public access
+	A(int b) {} // restrict the default access
+	private A(String s) {} // restrict the private access
 } // <--- this restricts the default access
+
+
+/* explanation of restriction
+ * 1. public access restriction: this allows any access from every package to call the constructor
+ * 2. Default access restriction: the constructor can't be called from other package.
+ * 3. Protect access restriction: the constructor can be called from other package if it is parents and child relationship.
+ * 4. private access restriction: any package can't access. only class insdie can call the constructor and object.
+ *
+ *
+ */
